9359d9ac89419f066e58ac994fa7adbffcc84cd3
Ahmad Samba Conteh <ahcon1@morgan.edu>
2019-11-16 13:26:11 -0500

    changed port

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def index():
     return flask.render_template("index.html")
 
 app.run(
-    port=int(os.getenv('PORT', 8080)),
-    host=os.getenv('IP', '0.0.0.0')
+    port=int(os.getenv('PORT', 8086)),
+    host=os.getenv('IP', '127.0.0.1')
 )
