bac368a7fe1908e6ad95aecf3915f80ab59f1259
deandraharvin <deandraharvin3@gmail.com>
2019-11-16 16:18:54 -0500

    Changing path for resources

--- app.py
+++ app.py
@@ -9,7 +9,16 @@ Bootstrap(app)
 def index():
     return flask.render_template("index.html")
 
+
+@app.route('/resources')
+def resources():
+    return flask.render_template("base.html")
+
+
 app.run(
     port=int(os.getenv('PORT', 8085)),
     host=os.getenv('IP', '127.0.0.1')
 )
+
+if __name__ == '__main__':
+    app.run(debug=True)
