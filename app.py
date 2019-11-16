d602fc08bd3c3d45b37ad6a5946a219d589c7dd8
deandraharvin <deandraharvin3@gmail.com>
2019-11-16 13:33:09 -0500

    Changing port

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # app.py
 import flask
 import os
+from flask_bootstrap import Bootstrap
 
 app = flask.Flask(__name__)
-
+Bootstrap(app)
 @app.route('/')
 def index():
     return flask.render_template("index.html")
 
 app.run(
-    port=int(os.getenv('PORT', 8080)),
-    host=os.getenv('IP', '0.0.0.0')
+    port=int(os.getenv('PORT', 8086)),
+    host=os.getenv('IP', '127.0.0.1')
 )
