375b092e7ea6ad15a259ce597d9aae2713d38e04
deandraharvin <deandraharvin3@gmail.com>
2019-11-16 14:47:34 -0500

    Adding template from bootstrap

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def index():
     return flask.render_template("index.html")
 
 app.run(
-    port=int(os.getenv('PORT', 8086)),
+    port=int(os.getenv('PORT', 8085)),
     host=os.getenv('IP', '127.0.0.1')
 )
