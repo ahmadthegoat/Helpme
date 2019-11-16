969bd0a5d410cedd15f8128dae9fc24d4a32b5c0
Ahmad Samba Conteh <ahcon1@morgan.edu>
2019-11-16 17:54:19 -0500

    food

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def index():
 def resources():
     return flask.render_template("base.html")
 
-
+@app.route('/contact')
+def contact():
+    return flask.render_template("contact.html")
 app.run(
     port=int(os.getenv('PORT', 8085)),
     host=os.getenv('IP', '127.0.0.1')
